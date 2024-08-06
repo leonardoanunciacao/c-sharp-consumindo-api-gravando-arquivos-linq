33150ab9d33d6feb02e15d71e86f9f63b662a48f
Leonardo Anunciação <leonardoalmeidaanunciacao@gmail.com>
2024-08-06 20:57:24 -0300

    Aula 01. Integrando uma API externa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.jso");
+        Console.WriteLine(resposta);
+    }
+    catch (System.Exception ex)
+    {
+        Console.WriteLine($"Ocorreu um problema: {ex.Message}");
+    }
+}
